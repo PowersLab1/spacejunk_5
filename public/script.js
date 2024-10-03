efdd44d2b84fdf04cdb7e742644bb6dd2fd0ff9d
Max Greenwald <maxsupergreenwald@gmail.com>
2024-10-03 18:20:18 -0400

    copied over files spacejunk_bl, changed package.json and config.json, and altered script.js testDataURL to new trialstructure

--- public/script.js
+++ public/script.js
@@ -64,7 +64,7 @@ export function thisTry(globalState, ctx) {
     }
 
     // The URL for the test data CSV
-    const testDataURL = "https://raw.githubusercontent.com/Powers-Lab/space_game_resources/main/pacetask_jitter_v2.csv"
+    const testDataURL = "https://raw.githubusercontent.com/Powers-Lab/space_game_resources/main/pacetask_jitter_v5.csv"
 
     // Get the Data for the test
     getData(testDataURL).then((testData) => {
